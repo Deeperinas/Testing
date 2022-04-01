6b4e9c7df33389633741ef56839c5b8318c874e0
Linas Skirmontas <linas.skirmontas@deeper.eu>
2022-04-01 19:14:55 +0300

    Initial commit, testuojam kaip veikia githubas

---
+++ CheckBoxDemo.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTestai
+{
+    class CheckBoxDemo
+    {
+        private static IWebDriver driver;
+
+        [OneTimeSetUp]
+        public static void OneTimeSetUp()
+        {
+            driver = new FirefoxDriver();
+            driver.Url = "http://demo.seleniumeasy.com/basic-checkbox-demo.html";
+            driver.Manage().Window.Maximize();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        }
+
+        [OneTimeTearDown]
+        public static void OneTimeTearDown()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public static void TestMultipleCheckBoxes()
+        {
+            IReadOnlyCollection<IWebElement> checkboxes = driver.FindElements(By.CssSelector(".cb-element"));
+
+            foreach (IWebElement checkbox in checkboxes)
+            {
+                checkbox.Click();
+            }
+
+
+        }
+
+
+    }
+
+}
---
+++ CheckBoxSingular.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTestai
+{
+    class CheckBoxSingular
+    {
+        private static IWebDriver driver;
+
+        [OneTimeSetUp]
+        public static void OneTimeSetUp()
+        {
+            driver = new FirefoxDriver();
+            driver.Url = "http://demo.seleniumeasy.com/basic-checkbox-demo.html";
+            driver.Manage().Window.Maximize();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        }
+
+        [OneTimeTearDown]
+        public static void OneTimeTearDown()
+        {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
+            driver.Quit();
+        }
+
+        [Test]
+        public static void TestSingularCheckbox()
+        {
+            driver.FindElement(By.Id("isAgeSelected")).Click();
+            IWebElement success = driver.FindElement(By.CssSelector("#txtAge"));
+
+            Assert.AreEqual("Success - Check box is checked", success.Text, "Kazkas ne taip");
+
+            
+
+
+        }
+
+
+    }
+
+}
